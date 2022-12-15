01d56063989a7f48c8c9f61240523ca2239a0f9f
Apple <apple@Sotanomakku.local>
2022-12-15 21:59:35 +0900

    管理者画面ポスト機能追加、html充実

--- posts/models.py
+++ posts/models.py
@@ -2,7 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-  title = models.CharField(max_length=180)
-  published = models.DateTimeField()
-  image = models.ImageField(upload_to="media/")
-  body = models.TextField()
+    title = models.CharField(max_length=180)
+    published = models.DateTimeField()
+    image = models.ImageField(upload_to="media/")
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:30]
